49459b2a1c3fedcdec387fcc1ee95342311951c7
ZHB-JOB <zhb20111503@126.com>
2018-08-20 18:02:03 +0800

    整合 SSH

--- webapp/src/main/java/com/zhb/vue/web/controller/LoginController.java
+++ webapp/src/main/java/com/zhb/vue/web/controller/LoginController.java
@@ -1,5 +1,23 @@
 package com.zhb.vue.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/logincontroller")
 public class LoginController {
+    
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
+    
+    @RequestMapping("/zhbController")
+    public String testController(HttpServletRequest request,HttpServletResponse response) {
+        logger.info("---------------zhb----------------------");
+        return "zhb.index";
+    }
 
 }
