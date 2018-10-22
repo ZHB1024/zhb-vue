935c485c1e421f7327cb3cdd65cfea18085b72ae
ZHB-JOB <zhb20111503@126.com>
2018-10-22 14:14:52 +0800

    WebApplicationContext getBean

--- vue/src/main/java/com/zhb/vue/service/ServiceFactory.java
+++ vue/src/main/java/com/zhb/vue/service/ServiceFactory.java
@@ -1,12 +1,16 @@
 package com.zhb.vue.service;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.zhb.vue.Constant;
 
 public class ServiceFactory {
     
     private static ClassPathXmlApplicationContext beanFac = new ClassPathXmlApplicationContext(Constant.APPLICATIONCONTEXT_CONF);
+    
+    private static WebApplicationContext wac = ContextLoader.getCurrentWebApplicationContext();
     
     public static Object getBean(String beanId){
         return beanFac.getBean(beanId);
@@ -20,6 +24,11 @@ public class ServiceFactory {
     public static AttachmentInfoService getAttachmentInfoService() {
         AttachmentInfoService attachmentInfoService = beanFac.getBean("attachmentInfoServiceImpl", AttachmentInfoService.class);
         return attachmentInfoService;
+    }
+    
+    public static DicInfoService getDicInfoService() {
+        DicInfoService dicInfoService = wac.getBean("dicInfoServiceImpl", DicInfoService.class);
+        return dicInfoService;
     }
 
 }
