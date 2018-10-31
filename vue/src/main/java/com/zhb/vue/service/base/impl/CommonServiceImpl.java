49a7a9713fb98996018a597ec9b666e6f781a373
ZHB-JOB <zhb20111503@126.com>
2018-10-31 17:28:02 +0800

    事务

--- vue/src/main/java/com/zhb/vue/service/base/impl/CommonServiceImpl.java
+++ vue/src/main/java/com/zhb/vue/service/base/impl/CommonServiceImpl.java
@@ -22,7 +22,6 @@ import com.zhb.vue.service.base.CommonService;
 */
 
 @Service
-@Transactional
 public class CommonServiceImpl implements CommonService{
 
     private Logger logger = LoggerFactory.getLogger(CommonServiceImpl.class);
@@ -31,56 +30,67 @@ public class CommonServiceImpl implements CommonService{
     public CommonDao commonDao;
     
     @Override
+    @Transactional
     public  <T> Serializable save(T entity) {
         return commonDao.save(entity);
     }
     
     @Override
+    @Transactional
     public  <T> void batchSave(List<T> entitys) {
         commonDao.batchSave(entitys);
     };
     
     @Override
+    @Transactional
     public <T> void updateEntitie(T pojo) {
         commonDao.updateEntitie(pojo);
     }
 
     @Override
+    @Transactional
     public <T> void updateEntityById(Class entityName, Serializable id) {
         commonDao.updateEntityById(entityName,id);
     }
 
     @Override
+    @Transactional
     public <T> void updateEntitie(String className, Object id) {
         commonDao.updateEntitie(className,id);
     };
     
     @Override
+    @Transactional
     public  <T> T saveOrUpdate(T entity) {
         return commonDao.saveOrUpdate(entity);
     };
 
     @Override
+    @Transactional
     public  <T> void delete(T entity) {
         commonDao.delete(entity);
     };
     
     @Override
+    @Transactional
     public  <T> void deleteEntityById(Class entityName, Serializable id) {
         commonDao.deleteEntityById(entityName,id);
     };
     
     @Override
+    @Transactional
     public  <T> void deleteAllEntitie(Collection<T> entities) {
         commonDao.deleteAllEntitie(entities);
     };
     
     @Override
+    @Transactional
     public  <T> T get(Class<T> entityClass, Serializable id) {
         return commonDao.get(entityClass, id);
     };
 
     @Override
+    @Transactional
     public  <T> T getEntity(Class entityName, Serializable id) {
         return commonDao.getEntity(entityName, id);
     }
